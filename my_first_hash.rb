0d097877b462d4d586019b98ce9a452eba3d2236
Hiren Mistry <hirenmst@gmail.com>
2020-07-15 02:17:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,8 +19,11 @@ def adding
   shipping_manifest = {
     "whale bone corsets" => 5, 
     "porcelain vases" => 2, 
-    "oil paintings" => 3}
+    "oil paintings" => 3
+    
+  }
   
   shipping_manifest["muskets"] = 2
+  shipping_manifest[]
   
 end
