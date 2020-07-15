55d0e04794d00355b425fd3706f2ed56c8700845
Hiren Mistry <hirenmst@gmail.com>
2020-07-15 02:20:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,6 @@ def adding
     "whale bone corsets" => 5, 
     "porcelain vases" => 2, 
     "oil paintings" => 3
-    
   }
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
